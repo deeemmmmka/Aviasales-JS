ebc7ea9497238cc682986bc9688786197f7bf981
deeemmmmka <deeemmmmka777@yandex.ru>
2020-03-18 13:51:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// собираем все необходимые инпуты и списки
 const formSearch = document.querySelector('.form-search'),
     inputCitiesFrom = document.querySelector('.input__cities-from'),
     dropDownCitiesFrom = document.querySelector('.dropdown__cities-from'),
@@ -5,10 +6,12 @@ const formSearch = document.querySelector('.form-search'),
     dropDownCitiesTo = document.querySelector('.dropdown__cities-to'),
     inputDateDepart = document.querySelector('.input__date-depart');
 
+// создаем массив городов
 const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Керчь', 'Симферополь', 'Волгоград', 'Самара', 'Днепропетровск', 'Екатеринбург', 'Одесса', 'Ухань', 'Нижний Новгород', 'Калининград', 'Вроцлав', 'Ростов-на-Дону', 'Киев', 'Владивосток', 'Нью-Йорк', 'Лондон', 'Тегеран', 'Канны', 'Оттава'];
 
+//помещаем в переменную функцию для выбора города
 const showCity = (input, list) => {
-    list.textContent = '';
+    list.textContent = ''; // очищаем выпадающее меню
 
     if(input.value !== ''){
 
@@ -26,13 +29,16 @@ const showCity = (input, list) => {
     }
 };
 
+// при наборе чего либо в инпуте города вылета вызываем функцию, помогающую выбрать город вылета
 inputCitiesFrom.addEventListener('input', () => {
     showCity(inputCitiesFrom, dropDownCitiesFrom)
 });
+// при наборе чего либо в инпуте города прилета вызываем функцию, помогающую выбрать город прилета
 inputCitiesTo.addEventListener('input', () => {
     showCity(inputCitiesTo, dropDownCitiesTo)
 });
 
+// помещаем в переменную функцию выбора города в выпадающем списке
 const targetPush = (input, list) => {
     const target = event.target;
     if(target.tagName.toLowerCase() === 'li'){
@@ -41,9 +47,12 @@ const targetPush = (input, list) => {
     }
 }
 
+// вешаем событие выбора нужного города вылета при клике на элемент выпадающего списка
 dropDownCitiesFrom.addEventListener('click', (event) => {
     targetPush(inputCitiesFrom, dropDownCitiesFrom)
 });
+
+// вешаем событие выбора нужного города прилета при клике на элемент выпадающего списка
 dropDownCitiesTo.addEventListener('click', (event) => {
     targetPush(inputCitiesTo, dropDownCitiesTo)
 });
